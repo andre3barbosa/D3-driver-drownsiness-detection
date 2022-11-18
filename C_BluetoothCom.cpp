d6202da90545434256d6e637d9b5f07362e0b4f0
Andre <a91989@alunos.uminho.pt>
2022-11-18 14:37:03 +0000

    C_BluetoothCom.cpp

---
+++ C_BluetoothCom.cpp
@@ -0,0 +1,28 @@
+#include "C_BluetoohCom.h"
+
+//debug
+#include <unistd.h>
+#include "utils.h"
+#include "defs.h"
+#include "debug.h"
+ 
+using namespace std;
+ 
+
+ C_BluetoohCom::C_BluetoohCom():
+
+ {
+andre
+ }
+
+ C_BluetoohCom::~C_BluetoohCom():
+
+ {
+
+ }
+
+ bool C_BluetoohCom::(string):
+
+ {
+
+ }
