0ddc14ccff6339847abde969dcca234e14f4b244
soaresV4 <73140303+soaresV4@users.noreply.github.com>
2023-01-24 16:40:37 +0000

    today

--- CDrowsinessCam.cpp
+++ CDrowsinessCam.cpp
@@ -1,178 +1,70 @@
 #include "CDrowsinessCam.h"
 
-#include <stdlib.h>     /* abs */
-#include <unistd.h>
-#include <iostream>
-#include <list>
-
-#include <chrono> //time
-
-
-
-
-using namespace std;
-using namespace cv;
-using namespace cv::face;
-using namespace cv::ml;
-
+//constructor
 CDrowsinessCam::CDrowsinessCam()
-    :faceDetector("/usr/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml"),
-    gray(),
-    p2_p6_final(0.0), 
-    p3_p5_final(0.0),
-    p1_p4_final(0.0)
 {
-   // Load landmark detector
-    facemark = FacemarkLBF::create();
-    facemark->loadModel("/usr/share/OpenCV/lbfmodel.yaml");
-    svm = SVM::load("/root/svm.xml");
-
-    next_class = NUM_NEXT_CLASS;
-
-
-    for(i=0;i<MAX_FEAT;i++)
-        classInput[i]=0.0;
-
-
-    long_blinks = 0;
-    total_blinks = 180;
-    short_blinks = 180;
-    consecutive_long_blinks = 0;
-    cout << "begin" << endl;
-
+    //reserve 15 elements to store EAR values
+    this->classInput.reserve(15);
+    this->classInput.clear();
+    this->alertNumber = 0;
 }
+
+//destructor
 CDrowsinessCam::~CDrowsinessCam()
 {
 
-}
-bool CDrowsinessCam::processParameter(Mat frame)
+}   
+
+//Get the EAR value and store in the 15 vector
+bool CDrowsinessCam::processParameter(/*MAT Lframe*/)
 {
+    float earVal = 0.0;
 
-        if (frame.empty())
-          cerr  << ("[Camera] blank frame captured.\n");
-        
-        // Find face
-        vector<Rect> faces;
-        // Convert frame to grayscale because
-        // faceDetector requires grayscale image.
-        cvtColor(frame, gray, COLOR_BGR2GRAY);
-
-        // Detect faces
-        faceDetector.detectMultiScale(gray, faces);
-        
-        // Variable for landmarks. 
-        // Landmarks for one face is a vector of points
-        // There can be more than one face in the image. Hence, we 
-        // use a vector of vector of points. 
-        vector< vector<Point2f> > landmarks;
-        //vector<Point2f> landmarks;
-        // Run landmark detector
-        bool success = facemark->fit(frame,faces,landmarks);
-                
-        if(success)
-        //{   
-            // If successful, render the landmarks on the face
-            for(int i = 0; i < landmarks.size(); i++)
-            {
-                this->renderFace(frame, landmarks[i]);
-            }
-        //}  
-
-}
-
-
-
-
-float CDrowsinessCam::EARcalculation()
-{     
-    double EAR = (p2_p6_final +  p3_p5_final)/(2* p1_p4_final);  
     
-    return (float)EAR; 
-}
-
-bool CDrowsinessCam::checkDrowState(float* input)
-{
-    Mat input_x = Mat(1, MAX_FEAT, CV_32F, input);
-    int output_y = svm->predict(input_x);
     
-    // takes true value for alarm, and false value doesn't alarm
-    bool alert_var = 0;
+    
 
-    cout << output_y << endl;
 
-    //rule verification
-    // Update blink count
-        if (output_y == 2) {
-            long_blinks++;
-            consecutive_long_blinks++;
-        }else if(output_y == 1){
-            short_blinks++;
-            consecutive_long_blinks = 0;
-        }else{
-            consecutive_long_blinks = 0;
-        }
-        total_blinks++;
 
-        // Add blink to the queue
-        blink_queue.push_back(output_y);
+    // //TO-DO insert in pos1 or 0?
+    // this->classInput.emplace(this->classInput.begin(), 1, earVal);
+    // int i=5;
+    // if((i--) == 0 && this->classInput.size()==15)
+    // {
 
-        // Check if queue size is greater than 60
-        if (blink_queue.size() > 180) {
-            // Remove oldest blink from the queue
-            int oldest_blink = blink_queue.front();
-            blink_queue.pop_front();
+    //     i = 5;
+    // }
 
-            // Check if oldest blink was a long blink
-            if (oldest_blink == 2) {
-                long_blinks--;
-                consecutive_long_blinks--;
-            }else if(oldest_blink == 1){
-                short_blinks--;
-            }
-            total_blinks--;
-        }
 
-        // Calculate proportion of long blinks
-        double proportion = (double)long_blinks / total_blinks;
-
-        // Check if proportion is higher than 25%
-        //in the first 60 seconds don't made
-
-        if (proportion > 0.25) {
-            std::cout << "sleep" << std::endl;
-            alert_var = true;
-        }else{
-            //std::cout << "wake" << std::endl;
-            alert_var = false;
-        }
-
-        if (consecutive_long_blinks >= 4) {
-            std::cout << "ALERTA 5 or more consecutive long blinks in the last 60 values." << std::endl;
-            alert_var = true;
-        }
-
-    return alert_var;
 }
 
-void CDrowsinessCam::renderFace(cv::Mat &frame, vector<Point2f> &landmarks)
+
+int CDrowsinessCam::EARcalculation()
 {
-    double p2_p6_x = landmarks[37].x  - landmarks[41].x; 
-    double p2_p6_y=  landmarks[37].y -landmarks[41].y ;
-
-    this->p2_p6_final=sqrt((p2_p6_x * p2_p6_x) + (p2_p6_y * p2_p6_y));
-
-    //p3-p5
-    double p3_p5_x = landmarks[38].x - landmarks[40].x;
-    double p3_p5_y = landmarks[38].y - landmarks[40].y;
-
-    this->p3_p5_final=sqrt((p3_p5_x * p3_p5_x) + (p3_p5_y * p3_p5_y));
-
-
-    //p1-p4
-    double p1_p4_x = landmarks[36].x - landmarks[39].x;  
-    double p1_p4_y = landmarks[36].y - landmarks[39].y;
-
-    this->p1_p4_final=sqrt((p1_p4_x * p1_p4_x) + (p1_p4_y * p1_p4_y));
 
 }
 
+
+//Reads the 15 values stored in global variable
+//Do the classification of the 15 EAR input
+//Store the result in a global variable
+bool CDrowsinessCam::checkDrowState()
+{
+    //only enters when the first 15 EAR values were captured
+    
+
+    /*if()    //if the 
+    {
+
+    }
+    if()
+    {
+
+    }*/
+    //increment alert number
+}
+
+int CDrowsinessCam::getAlertNumber() const
+{
+    return this->alertNumber;
+}
--- CDrowsinessCam.h
+++ CDrowsinessCam.h
@@ -1,79 +1,24 @@
-#include <stdlib.h>     /* abs */
-#include <unistd.h>
-
-#include "/usr/include/opencv2/opencv.hpp"
-#include "/usr/include/opencv2/face.hpp"
-#include "/usr/include/opencv2/ml.hpp"
-
-#include <iostream>
-#include <fstream>
-
-#include <deque>
-
-
-#define MAX_FEAT 15
-#define NUM_NEXT_CLASS 4
-
-using namespace std;
-using namespace cv;
-using namespace cv::face;
-using namespace cv::ml;
-
-
+#include <vector>
 
 class CDrowsinessCam
 {
 private:
     int drowLevel;
-    //bool calculateDrowLevel();
+    //Global value to store the 15 EAR values
+    //that will be the input of the svm classification
+    std::vector<float> classInput;
 
-    void drawPolyline( Mat &frame, const vector<Point2f> &landmarks, const int start, const int end, bool isClosed = false);
-    void renderFace(cv::Mat &frame, vector<Point2f> &landmarks);
+    int EARcalculation();   //calculate the value of EAR
 
-    CascadeClassifier faceDetector;
-    Ptr<Facemark> facemark;
-    Mat gray;
-
-    Ptr<SVM> svm;
-
-    //Rule variables
-    int long_blinks;
-    int total_blinks;
-    int short_blinks;
-    int consecutive_long_blinks;
-    std::deque<int> blink_queue;
-
-
-    //para calculo do ear
-    double p2_p6_final,  p3_p5_final,  p1_p4_final;
-
-    int i;
-    float classInput[MAX_FEAT];
-    ofstream file;
-
-    int next_class;
-
-    std::chrono::steady_clock::time_point begin;
-    std::chrono::steady_clock::time_point end;
-    
+    int alertNumber;
 
 public:
     CDrowsinessCam();
     ~CDrowsinessCam();
-
-
     
-    bool processParameter(Mat frame);  //Obtain
-    float EARcalculation();  //calculate the value of EAR
+    bool processParameter(/*MAT Lframe*/);  //Obtain
 
-    bool checkDrowState(float *);  //machine learning algorithm
+    bool checkDrowState();  //machine learning algorithm
 
-
-
-
-
-
-
-
-
-};
+    int getAlertNumber() const;
+};  
